60d0b722489ea59fc9ac1e4fc597db5becb4fdcf
juangh15 <47037988+juangh15@users.noreply.github.com>
2022-05-05 09:05:48 -0500

    Added Item schema

---
+++ schema/Item.schema.js
@@ -0,0 +1,25 @@
+const itemSchema = {
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "integer"
+      },
+      "name": {
+        "type": "string"
+      },
+      "sellIn": {
+        "type": "integer"
+      },
+      "quality": {
+        "type": "integer"
+      },
+      "type": {
+        "type": "string"
+      },
+      "expired": {
+        "type": "boolean"
+      }
+    }
+  }
+
+exports.itemSchema = itemSchema;
